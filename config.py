61ae3c7ee68d49f94bd4a8a7e44b225d50856537
ziluan <1120047212@qq.com>
2026-04-23 14:44:45 +0800

    chore: update configs and datasets

--- config.py
+++ config.py
@@ -3,16 +3,29 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY = os.environ["API_KEY"]
+EMBEDDING_API_KEY = os.getenv("EMBEDDING_API_KEY")
+LLM_API_KEY = os.getenv("LLM_API_KEY")
 API_BASE = "https://oneapi.qunhequnhe.com/v1"
 
-TEXT_MODEL = "bge-m3-1024d"
-TEXT_DIM = 1024
+RERANK_API_KEY = os.getenv("RERANK_API_KEY")
 
-IMAGE_MODEL = "img-embedding-cnclip-yuntu-ft-768d"
-IMAGE_DIM = 768
+QDRANT_PATH = "./qdrant_storage"
+
+LLM_MODEL = 'qwen3.5-plus'
+
+OFFLINE_MATERIALS_JSON = "data/offline_materials/offline_materials.json"
+ONLINE_QUERY_TEXT_IMAGES_JSON = "data(test)/online_querys/online_query.json"
+OUTPUT_PATH = "./output/multimodal_rag/answer.md"
+
+MULTIMODAL_MODEL = "qwen3-vl-embedding"
+MULTIMODAL_DIM = 2560
+
+# MULTIMODAL_MODEL = "doubao-embedding-vision"
+# MULTIMODAL_DIM = 1024
+
+RERANK_MODEL = "qwen3-vl-rerank"
 
 TEXT_COLLECTION = "text_collection"
 IMAGE_COLLECTION = "image_collection"
 
-QDRANT_PATH = "./qdrant_storage"
+RERANKER_MODEL_PATH = "./qwen/Qwen3-VL-Reranker-8B"
